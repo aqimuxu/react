964a22f2931157f9007818b5357e4de47e4fcb39
aqimuxu <qinying1130@163.com>
2022-08-03 22:44:50 +0800

    设置移动端页面的适配

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
 import classes from './App.module.css'
 const App = () => {
-    const [showBorder,setShowBorder]=useState(false)
-    return (
-        <div>
-            <p className={`${classes.p1} ${showBorder?classes.border:''}`}>我是段落</p>
-            <button onClick={()=>setShowBorder(true)}>点我</button>
+    return(
+        <div style={{width:'750rem',height:200,backgroundColor:'#bfa'}}>
+
         </div>
-    );
+    )
 };
 
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import './index.css'
+//设置移动端的适配
+//视口为750rem,如果width设为75rem就是整个宽度的十分之一
+document.documentElement.style.fontSize=100/750+'vm'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
