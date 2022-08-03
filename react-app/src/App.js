94366e9fd79b1e3129e6a5eb17e49c459c0470f1
aqimuxu <qinying1130@163.com>
2022-08-03 19:59:30 +0800

    CSS module

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import {useState} from "react";
-import './App.css'
+import React, {useState} from 'react';
+import classes from './App.module.css'
 const App = () => {
-    const [redBorder,setRedBorder]=useState(false)
-    // const pStyle={color:'red',
-    //     border:redBorder?'red solid 1px':'blue solid 1px',
-    //     backgroundColor:'#bfa',
-    // }
-    const changeHandler=()=>{
-        setRedBorder(true)
-    }
+    const [showBorder,setShowBorder]=useState(false)
     return (
         <div>
-            <p className={`p1 ${redBorder?'':'blueBorder'}`}>我是段落</p>
-            <button onClick={changeHandler}>点我</button>
+            <p className={`${classes.p1} ${showBorder?classes.border:''}`}>我是段落</p>
+            <button onClick={()=>setShowBorder(true)}>点我</button>
         </div>
     );
 };
