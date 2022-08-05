a1e5ba72ed3b39c8d63002dc5b83430941a0c3ea
aqimuxu <qinying1130@163.com>
2022-08-05 15:55:40 +0800

    加载数据

--- react-app/src/Components/Meals/Meal/Meal.js
+++ react-app/src/Components/Meals/Meal/Meal.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import classes from "./Meal.module.css";
 import Counter from "../../UI/Counter/Counter";
-const Meal = () => {
+const Meal = (props) => {
     return (
         <div className={classes.Meal}>
             <div className={classes.ImgBox}>
-                <img src="/img/meals/1.png" />
+                <img src={props.meal.img} />
             </div>
             <div>
-                <h2 className={classes.Title}>汉堡</h2>
-                <p className={classes.Desc}>百分百纯牛肉配搭爽脆酸瓜洋葱粒与美味番茄酱经典滋味让你无法抵挡！</p>
+                <h2 className={classes.Title}>{props.meal.title}</h2>
+                <p className={classes.Desc}>{props.meal.desc}</p>
                 <div className={classes.PriceWrap}>
-                    <span className={classes.Price}>12</span>
-                    <Counter amount={0}/>
+                    <span className={classes.Price}>{props.meal.price}</span>
+                    <Counter amount={1}/>
                 </div>
             </div>
         </div>
--- react-app/src/Components/Meals/Meals.js
+++ react-app/src/Components/Meals/Meals.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import Meal from "./Meal/Meal";
 import classes from "./Meals.module.css";
-const Meals = () => {
+const Meals = (props) => {
     return (
         //将滚动条设置给了Meals
-        <div className={classes.Meals}>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
-            <Meal/>
+        <div className={classes.Meals}>{
+            props.mealsData.map(item=>
+                <Meal key={item.id} meal={item}/>
+            )
+        }
         </div>
     );
 };
